1ed220282d05916be348ade4d594ca9da4249900
Mikio Crosby <mikio.crosby@gmail.com>
2021-11-12 18:57:23 +0900

    test: converted team images to svg

--- src/data/advisors.js
+++ src/data/advisors.js
@@ -3,31 +3,57 @@ import Ning from '../../public/assets/Team/ning.jpeg';
 import Ayush from '../../public/assets/Team/ayush.png';
 import Elsa from '../../public/assets/Team/elsa.png';
 
+// const Advisors = [
+//   {
+//     name: 'Wei',
+//     position: '',
+//     twitter: '',
+//     pic: Wei,
+//   },
+//   {
+//     name: 'Ning',
+//     position: '',
+//     twitter: '',
+//     pic: Ning,
+//   },
+//   {
+//     name: 'Ayush',
+//     position: '',
+//     twitter: '',
+//     pic: Ayush,
+//   },
+//   {
+//     name: 'Elsa',
+//     position: '',
+//     twitter: '',
+//     pic: Elsa
+//   }
+// ]
+
 const Advisors = [
   {
     name: 'Wei',
     position: '',
     twitter: '',
-    pic: Wei,
+    pic: 'wei',
   },
   {
     name: 'Ning',
     position: '',
     twitter: '',
-    pic: Ning,
+    pic: 'ning',
   },
   {
     name: 'Ayush',
     position: '',
     twitter: '',
-    pic: Ayush,
+    pic: 'ayush',
   },
   {
     name: 'Elsa',
     position: '',
     twitter: '',
-    pic: Elsa
+    pic: 'elsa',
   }
 ]
-
 export default Advisors;
--- src/data/team.js
+++ src/data/team.js
@@ -8,72 +8,140 @@
 // import Doug from '../../public/assets/Team/doug.jpeg';
 // import Clare from '../../public/assets/Team/clare.png';
 
-import Bill from '../../teamimages/bill.png';
-import Leo from '../../teamimages/leo.jpeg';
-import Mikio from '../../teamimages/mikio.png';
-import Edward from '../../teamimages/edward.png';
-import Ada from '../../teamimages/ada.png';
-import Harold from '../../teamimages/harold.jpeg';
-import Masa from '../../teamimages/masa.png';
-import Doug from '../../teamimages/doug.jpeg';
-import Clare from '../../teamimages/clare.png';
+// import Bill from '../../teamimages/bill.png';
+// import Leo from '../../teamimages/leo.jpeg';
+// import Mikio from '../../teamimages/mikio.png';
+// import Edward from '../../teamimages/edward.png';
+// import Ada from '../../teamimages/ada.png';
+// import Harold from '../../teamimages/harold.jpeg';
+// import Masa from '../../teamimages/masa.png';
+// import Doug from '../../teamimages/doug.jpeg';
+// import Clare from '../../teamimages/clare.png';
 
+import Bill from '../../public/assets/Team/bill.svg';
+import Leo from '../../public/assets/Team/leo.svg';
+import Mikio from '../../public/assets/Team/mikio.svg';
+import Edward from '../../public/assets/Team/edward.svg';
+import Ada from '../../public/assets/Team/ada.svg';
+import Harold from '../../public/assets/Team/harold.svg';
+import Masa from '../../public/assets/Team/masa.svg';
+import Doug from '../../public/assets/Team/doug.svg';
+import Clare from '../../public/assets/Team/clare.svg';
+
+
+// const Team = [
+//   {
+//     name: 'Bill',
+//     position: 'CEO',
+//     twitter: 'haha',
+//     pic: Bill,
+//   },
+//   {
+//     name: 'Leo',
+//     position: 'Chief Product & Strategy Officer',
+//     twitter: 'hhehe',
+//     pic: Leo,
+//   },
+//   {
+//     name: 'Mikio',
+//     position: 'Co-Founder',
+//     twitter: 'lol',
+//     pic: Mikio,
+//   },
+//   {
+//     name: 'Edward',
+//     position: 'Architect & Development Lead',
+//     twitter: 'nope',
+//     pic: Edward,
+//   },
+//   {
+//     name: 'Ada',
+//     position: 'Wallet & Blockchain Development Lead',
+//     twitter: 'nope',
+//     pic: Ada,
+//   },
+//   {
+//     name: 'Harold',
+//     position: 'Project Manager',
+//     twitter: 'nope',
+//     pic: Harold,
+//   },
+//   {
+//     name: 'Masa',
+//     position: 'Investor & Advisor',
+//     twitter: 'nope',
+//     pic: Masa,
+//   },
+//   {
+//     name: 'Doug',
+//     position: 'Marketing Lead',
+//     twitter: 'nope',
+//     pic: Doug,
+//   },
+//   {
+//     name: 'Clare',
+//     position: 'Marketing Specialist',
+//     twitter: 'nope',
+//     pic: Clare,
+//   }
+// ]
 
 const Team = [
   {
     name: 'Bill',
     position: 'CEO',
     twitter: 'haha',
-    pic: Bill,
+    pic: 'bill',
   },
   {
     name: 'Leo',
     position: 'Chief Product & Strategy Officer',
     twitter: 'hhehe',
-    pic: Leo,
+    pic: 'leo',
   },
   {
     name: 'Mikio',
     position: 'Co-Founder',
     twitter: 'lol',
-    pic: Mikio,
+    pic: 'mikio',
   },
   {
     name: 'Edward',
     position: 'Architect & Development Lead',
     twitter: 'nope',
-    pic: Edward,
+    pic: 'edward',
   },
   {
     name: 'Ada',
     position: 'Wallet & Blockchain Development Lead',
     twitter: 'nope',
-    pic: Ada,
+    pic: 'ada',
   },
   {
     name: 'Harold',
     position: 'Project Manager',
     twitter: 'nope',
-    pic: Harold,
+    pic: 'harold',
   },
   {
     name: 'Masa',
     position: 'Investor & Advisor',
     twitter: 'nope',
-    pic: Masa,
+    pic: 'masa',
   },
   {
     name: 'Doug',
     position: 'Marketing Lead',
     twitter: 'nope',
-    pic: Doug,
+    pic: 'doug'
   },
   {
     name: 'Clare',
     position: 'Marketing Specialist',
     twitter: 'nope',
-    pic: Clare,
+    pic: 'clare',
   }
 ]
+
 
 export default Team;
